b07e061d210315ab07cdc8853acf5ebd8713849e
5tirner <thatfor1337@gmail.com>
2024-01-09 04:03:48 +0100

    @5tirner

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+service php7.4-fpm start
 ./wp-cli.phar core download --allow-root
 sleep 1
-./wp-cli.phar config create --dbname=mydb --dbuser=zasabri --dbpass=Zs1234.. --dbhost=mariadb --allow-root
+./wp-cli.phar config create --dbname=wpdb --dbuser=zasabri --dbpass=Zs1234.. --dbhost=mariadb --allow-root
 sleep 1
 ./wp-cli.phar core install --url=zasabri.42.fr --title=Inception42 --admin_user=molchi --admin_password=Sz2001.. --admin_email=molchi123@gmail.com --allow-root
 sleep 1
 php-fpm7.4 -R -F
-#while true; do sleep 1; done;
+while true; do sleep 1; done;

b3cdcfd3134985bfb7cd462c6ede6e025637580a
5tirner <thatfor1337@gmail.com>
2024-01-11 04:22:47 +0100

    @5tirner

--- srcs/requirements/mariadb/tools/mysql.sh
+++ srcs/requirements/mariadb/tools/mysql.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 service mariadb start
-echo $DBNAME
-mysql -e "create database if not exists wordpressdb;"
+mysql -e "create database if not exists wpdb;"
 mysql -e "create user if not exists zasabri@'%' identified by 'Zs1234..';"
 #@% To give the user the rights to connects from any host
-mysql -e "grant all privileges on  wordpressdb.* to zasabri@'%';"
+mysql -e "grant all privileges on  wpdb.* to zasabri@'%';"
 mysql -e "flush privileges;"
 mysql

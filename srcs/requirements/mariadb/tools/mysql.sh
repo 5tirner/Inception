038beaed770672c8a514c00b32c28eb25c507072
5tirner <thatfor1337@gmail.com>
2024-01-11 04:21:49 +0100

    @5tirner

--- srcs/requirements/mariadb/tools/mysql.sh
+++ srcs/requirements/mariadb/tools/mysql.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 service mariadb start
-mysql -e "create database if not exists wpdb;"
+echo $DBNAME
+mysql -e "create database if not exists wordpressdb;"
 mysql -e "create user if not exists zasabri@'%' identified by 'Zs1234..';"
 #@% To give the user the rights to connects from any host
-mysql -e "grant all privileges on  wpdb.* to zasabri@'%';"
+mysql -e "grant all privileges on  wordpressdb.* to zasabri@'%';"
 mysql -e "flush privileges;"
 mysql
--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 service php7.4-fpm start
 ./wp-cli.phar core download --allow-root
-sleep 1
-./wp-cli.phar config create --dbname=wpdb --dbuser=zasabri --dbpass=Zs1234.. --dbhost=mariadb --allow-root
-sleep 1
-./wp-cli.phar core install --url=zasabri.42.fr --title=Inception42 --admin_user=molchi --admin_password=Sz2001.. --admin_email=molchi123@gmail.com --allow-root
-sleep 1
+./wp-cli.phar config create --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --dbhost=$DBHOST --allow-root
+./wp-cli.phar core install --url=$URL --title=$TITLE --admin_user=$ADMIN --admin_password=$ADMINPASS --admin_email=$ADMINMAIL --allow-root
 php-fpm7.4 -R -F

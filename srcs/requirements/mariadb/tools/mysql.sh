bfd6d3a4ff1a94bcf96f9290db2149751c618c25
5tirner <thatfor1337@gmail.com>
2024-01-13 04:19:57 +0100

    @5tirner

--- srcs/requirements/mariadb/tools/mysql.sh
+++ srcs/requirements/mariadb/tools/mysql.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 service mariadb start
-mysql -e "create database if not exists wpdb;"
-mysql -e "create user if not exists zasabri@'%' identified by 'Zs1234..';"
+mariadb -e "create database if not exists wpdb;"
+mariadb -e "create user if not exists zasabri@'%' identified by 'Zs1234..';"
 #@% To give the user the rights to connects from any host
-mysql -e "grant all privileges on  wpdb.* to zasabri@'%';"
-mysql -e "flush privileges;"
-mysql
+mariadb -e "grant all privileges on  wpdb.* to zasabri@'%';"
+mariadb -e "flush privileges;"
+#while true; do echo .; done;
--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+service php7.4-fpm start
 ./wp-cli.phar core download --allow-root
 sleep 3
 ./wp-cli.phar config create --dbname=wpdb --dbuser=zasabri --dbpass=Zs1234.. --dbhost=mariadb --allow-root
 sleep 3
 ./wp-cli.phar core install --url=zasabri.42.fr --title=Inception42 --admin_user=molchi --admin_password=Sz2001.. --admin_email=molchi123@gmail.com --allow-root
 sleep 3
-service php7.4-fpm start
 while true; do echo .; done;

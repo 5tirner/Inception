e85241bc47dc57676b372b96b9aff3d4277ac540
5tirner <thatfor1337@gmail.com>
2024-01-16 12:43:58 +0100

    @5tirner

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wp-cli.phar core download --allow-root
 wp-cli.phar config create --dbname=wpdb --dbuser=zasabri --dbpass=Zs1234.. --dbhost=mariadb --allow-root
-wp-cli.phar core install --url=zasabri.42.fr --title=Inception42 --admin_user=molchi --admin_password=Sz2001.. --admin_email=molchi123@gmail.com --allow-root
-php-fpm7.4
+wp-cli.phar core install --url=https://zasabri.42.fr --title=Inception42 --admin_user=molchi --admin_password=Sz2001.. --admin_email=molchi123@gmail.com --allow-root
+php-fpm7.4 -F -R

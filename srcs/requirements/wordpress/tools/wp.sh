71c03e095374e1047edb52263235b1d3bd471ee5
5tirner <thatfor1337@gmail.com>
2024-01-19 18:58:41 +0100

    @5tirner

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -2,4 +2,5 @@
 wp-cli.phar core download --allow-root
 wp-cli.phar config create --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --dbhost=mariadb --allow-root
 wp-cli.phar core install --url=$URL --title=$TITLE --admin_user=$ADMIN --admin_password=$ADMINPASS --admin_email=$ADMINMAIL --allow-root
+wp-cli.phar user create $USER $USER@gmail.com --role=subscriber --user_pass=$USERPASS --allow-root
 php-fpm7.4 -F -R

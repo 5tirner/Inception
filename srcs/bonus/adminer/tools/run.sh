5f4de4d408438b3eefc2864a500ad1ee531d6ead
5tirner <thatfor1337@gmail.com>
2024-01-19 16:01:21 +0100

    @5tirner

--- srcs/bonus/adminer/tools/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-service apache2 start
-service mariadb start
-ln -s   /etc/apache2/conf-available/adminer.conf /etc/apache2/conf-enabled/
-service apache2 restart
-while true; do sleep 1; done;
